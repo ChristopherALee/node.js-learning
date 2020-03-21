d4fb17ae94c51813df9483723bc1ed0ed698889f
ChristopherALee <christopheradamlee@gmail.com>
2020-03-21 16:38:13 -0400

    section 5

---
+++ u_node_js_dev_course/How-To.js
@@ -0,0 +1,8 @@
+// insert debugger
+
+// cmd: node inspect [filename] [cmd] [params]
+// ex. node inspect app.js add --title="Note 1" --body="Body 1"
+
+// chrome://inspect
+// check configuration
+// add file you want to debug to the chrome filesystem
